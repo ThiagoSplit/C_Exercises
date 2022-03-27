7c405840e48381033c84f32d13ab6df4a80af91b
ThiagoSplit <99969595+ThiagoSplit@users.noreply.github.com>
2022-03-27 16:33:44 -0300

    Update main.c

--- Exercícios/main.c
+++ Exercícios/main.c
@@ -1,8 +1,3 @@
-/*Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
-	a. o produto do dobro do primeiro com metade do segundo.
-	b. a soma do triplo do primeiro com o terceiro.
-	c. o terceiro elevado ao cubo.*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
